68e3d0957da9b32a1261513fd2841505b18f4699
Chrispy <chrisj21283@gmail.com>
2024-04-25 20:56:04 -0400

    create starter components and beginning API call

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,35 +1,42 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+// import components
+import Home from './components/Home';
+import Header from './components/Header'
+import Footer from './components/Footer'
+import './index.css'
 
-function App() {
-  const [count, setCount] = useState(0)
+const componentsByPage = {
+ Homepage: Home
+};
 
+const App = () => {
+  const [currentPage, setCurrentPage] = useState('Homepage');
+
+  const handleNavigationClick = (page) => {
+    setCurrentPage(page);
+  };
+  const CurrentComponent = componentsByPage[currentPage];
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
-}
+      <Header handleNavigationClick={handleNavigationClick} />
+   
+      <main>
+  <h1>Weather App Homepage from App.jsx</h1>
+        {CurrentComponent && <CurrentComponent />}
+      </main>
+    
+      <Footer />
+      </>
+);
+  
+};
 
 export default App
+
+
+
+
+
+
+
+
